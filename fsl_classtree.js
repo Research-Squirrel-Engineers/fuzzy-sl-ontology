664d8a0105674bed77f5789d1d6cec5d496bd5b9
florianthiery <mail@fthiery.de>
2023-12-12 13:55:03 +0000

    Deploying to gh-pages from @ Research-Squirrel-Engineers/fuzzy-sl-ontology@acca63a919e58103a91caa19d96fba2238e91bcb 🚀

--- fsl_classtree.js
+++ fsl_classtree.js
@@ -201,12 +201,8 @@ var tree={
           "from": {
             "http://www.w3.org/1999/02/22-rdf-syntax-ns#type": {
               "instancecount": 0,
-              "http://www.w3.org/2002/07/owl#NamedIndividual": 3,
-              "http://fuzzy-sl.squirrel.link/ontology/MethodType": 3
-            },
-            "http://www.w3.org/2000/01/rdf-schema#range": {
-              "instancecount": 0,
-              "http://www.w3.org/2002/07/owl#ObjectProperty": 1
+              "http://www.w3.org/2002/07/owl#NamedIndividual": 5,
+              "http://fuzzy-sl.squirrel.link/ontology/SourceType": 5
             }
           }
         },
